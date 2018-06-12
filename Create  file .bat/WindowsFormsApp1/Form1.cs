ca9d129f16ddd62b0835029a0f36d9dfc4798981
nowert <30897015+nowert@users.noreply.github.com>
2018-06-11 19:09:00 -0700

    Add files via upload

---
+++ Create  file .bat/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText("E:\\1. work\\ฝีกงาน\\test file.bat", "@echo off \n set path=%1 %2 %3\n");
+        }
+    }
+}
