162eace1a792a115293780307e3787026688a1ef
nowert <30897015+nowert@users.noreply.github.com>
2018-06-11 19:10:33 -0700

    Add files via upload

---
+++ Create  file .bat 1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKIOSK = new System.Windows.Forms.Button();
+            this.btnPEAQClient = new System.Windows.Forms.Button();
+            this.btnServer = new System.Windows.Forms.Button();
+            this.btnmove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKIOSK
+            // 
+            this.btnKIOSK.Location = new System.Drawing.Point(52, 110);
+            this.btnKIOSK.Name = "btnKIOSK";
+            this.btnKIOSK.Size = new System.Drawing.Size(151, 42);
+            this.btnKIOSK.TabIndex = 0;
+            this.btnKIOSK.Text = "Create File KIOSK";
+            this.btnKIOSK.UseVisualStyleBackColor = true;
+            this.btnKIOSK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPEAQClient
+            // 
+            this.btnPEAQClient.Location = new System.Drawing.Point(52, 173);
+            this.btnPEAQClient.Name = "btnPEAQClient";
+            this.btnPEAQClient.Size = new System.Drawing.Size(151, 42);
+            this.btnPEAQClient.TabIndex = 1;
+            this.btnPEAQClient.Text = "Create File PEAQClient";
+            this.btnPEAQClient.UseVisualStyleBackColor = true;
+            this.btnPEAQClient.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnServer
+            // 
+            this.btnServer.Location = new System.Drawing.Point(52, 47);
+            this.btnServer.Name = "btnServer";
+            this.btnServer.Size = new System.Drawing.Size(151, 42);
+            this.btnServer.TabIndex = 2;
+            this.btnServer.Text = "Create File Server";
+            this.btnServer.UseVisualStyleBackColor = true;
+            this.btnServer.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnmove
+            // 
+            this.btnmove.Location = new System.Drawing.Point(219, 47);
+            this.btnmove.Name = "btnmove";
+            this.btnmove.Size = new System.Drawing.Size(129, 168);
+            this.btnmove.TabIndex = 3;
+            this.btnmove.Text = "Move Folder";
+            this.btnmove.UseVisualStyleBackColor = true;
+            this.btnmove.Click += new System.EventHandler(this.btnmove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 262);
+            this.Controls.Add(this.btnmove);
+            this.Controls.Add(this.btnServer);
+            this.Controls.Add(this.btnPEAQClient);
+            this.Controls.Add(this.btnKIOSK);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKIOSK;
+        private System.Windows.Forms.Button btnPEAQClient;
+        private System.Windows.Forms.Button btnServer;
+        private System.Windows.Forms.Button btnmove;
+    }
+}
+
---
+++ Create  file .bat 1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\1. work\ฝีกงาน\test file KIOSK.bat";
+
+                File.Create(path).Dispose();
+
+                using (TextWriter create = new StreamWriter(path))
+                {
+                create.WriteLine("@echo off");
+                create.WriteLine("set path=%1 %2 %3");
+                create.WriteLine(":วนลูปตัดค่าเป็น 2 ค่า");
+                create.WriteLine("for /f "+"\"tokens=1* delims=-\""+" %%a in ("+"\"%path%\""+ ") do (");
+                create.WriteLine("set partF=%%a");
+                create.WriteLine("set partD=%%b");
+                create.WriteLine(")");
+                create.WriteLine(":ตัดค่าช่องว่างด้านหลัง"+" drive");
+                create.WriteLine("set partD=%partD%##");
+                create.WriteLine("set partD=%partD:                ##=##%");
+                create.WriteLine("set partD=%partD:        ##=##%");
+                create.WriteLine("set partD=%partD:    ##=##%");
+                create.WriteLine("set partD=%partD:  ##=##%");
+                create.WriteLine("set partD=%partD: ##=##%");
+                create.WriteLine("set partD=%partD:##=%");
+                create.WriteLine("");
+                create.WriteLine("RMDIR /s /q "+ "\""+"%partF%\\PEA_Kiosk\\"+ "\"");
+                create.WriteLine("");
+                create.WriteLine("cd /d %partF%\\PEA_Kiosk");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+"\""+"%partD%PEASmartQ\\PEASmartQ\\Program\\PEA_Kiosk_USB "+"\"" +"\""+"%partF%\\PEA_Kiosk\\"+"\""+" /e /y");
+                create.WriteLine("");
+                create.WriteLine("del "+"\"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\*Kiosk_Project *.lnk\"");
+                create.WriteLine("del "+"\"C:\\Documents and Settings\\All Users\\Start Menu\\Programs\\Startup\\*Kiosk_Project *.lnk\"");
+                create.WriteLine("del "+"\" % USERPROFILE %\\Desktop\\*Kiosk_Project *.lnk\"");
+                create.WriteLine("del "+"\" % USERPROFILE %\\Start Menu\\Programs\\Startup\\*Kiosk_Project *.lnk\"");
+                create.WriteLine("");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+"\" %partD%PEASmartQ\\PEASmartQ\\Program\\Shortcut\\CutPEA_Kiosk_Project.lnk\""+"\" C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\""+" /s /y ");
+                create.WriteLine("");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+"\" %partD%PEASmartQ\\PEASmartQ\\Program\\Shortcut\\CutPEA_Kiosk_Project.lnk\""+"\" C:\\Documents and Settings\\All Users\\Start Menu\\Programs\\Startup\\\""+" /s /y");
+                create.WriteLine("");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+"\" %partD%PEASmartQ\\PEASmartQ\\Program\\Shortcut\\CutPEA_Kiosk_Project.lnk\""+"\" %USERPROFILE%\\Desktop\\\""+" /s /y");
+                create.WriteLine("");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+"\" %partD%PEASmartQ\\PEASmartQ\\Program\\Shortcut\\CutPEA_Kiosk_Project.lnk\""+"\" %USERPROFILE%\\Start Menu\\Programs\\Startup\""+" /s /y");
+                create.WriteLine("");
+                create.WriteLine("start "+"\"\""+"\"%USERPROFILE%\\Start Menu\\Programs\\Startup\\CutPEA_Kiosk_Project.exe\""+" /s /y");         
+                create.Close();
+                }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\1. work\ฝีกงาน\test file PEAQClient.bat";
+
+            File.Create(path).Dispose();
+
+            using (TextWriter create = new StreamWriter(path))
+            {
+                create.WriteLine("@echo off");
+                create.WriteLine("set path=%1 %2 %3");
+                create.WriteLine(":วนลูปตัดค่าเป็น 2 ค่า");
+                create.WriteLine("for /f " + "\"tokens=1* delims=-\"" + " %%a in (" + "\"%path%\"" + ") do (");
+                create.WriteLine("set partF=%%a");
+                create.WriteLine("set partD=%%b");
+                create.WriteLine(")");
+                create.WriteLine(":ตัดค่าช่องว่างด้านหลัง" + " drive");
+                create.WriteLine("set partD=%partD%##");
+                create.WriteLine("set partD=%partD:                ##=##%");
+                create.WriteLine("set partD=%partD:        ##=##%");
+                create.WriteLine("set partD=%partD:    ##=##%");
+                create.WriteLine("set partD=%partD:  ##=##%");
+                create.WriteLine("set partD=%partD: ##=##%");
+                create.WriteLine("set partD=%partD:##=%");
+                create.WriteLine("");
+                create.WriteLine("RMDIR /s /q " + "\"" + "%partF%\\PEA_Kiosk\\" + "\"");
+                create.WriteLine("");
+                create.WriteLine("cd /d %partF%\\PEAQClient");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+"\" %partD%PEASmartQ\\PEASmartQ\\Program\\PEAQClient\""+ "\"%partF%\\PEAQClient\\\""+ " /e /y");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+"\" %partD%PEASmartQ\\PEASmartQ\\Program\\PEA_Server\\QPatchClient\""+ "\"%partF%\\QPatchClient\\\""+" /e /y");
+                create.WriteLine("");
+                create.WriteLine("cd /d C:\\Program Files\\");
+                create.WriteLine("md Avanade\\BPMClient\\Queuedata");
+                create.WriteLine("");
+                create.WriteLine("del "+"\"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\*STK - M01 *.lnk\"");
+                create.WriteLine("del "+"\"C:\\Documents and Settings\\All Users\\Start Menu\\Programs\\Startup\\*STK - M01 *.lnk\"");
+                create.WriteLine("del "+"\" %USERPROFILE%\\*STK - M01 *.lnk\"");
+                create.WriteLine("del "+"\" %USERPROFILE%\\Start Menu\\Programs\\Startup\\*STK - M01 *.lnk\"");
+                create.WriteLine("Start "+"\" %USERPROFILE%\\destop\\PEAQClient.exe\"");
+                create.WriteLine("");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+ "\" %partD%PEASmartQ\\PEASmartQ\\Program\\Shortcut\\PEAQClient.lnk\""+ "\"%USERPROFILE%\\Desktop\\\""+" /s /y");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+ "\" %partD%PEASmartQ\\PEASmartQ\\Program\\Shortcut\\QPatchClient.lnk\""+ "\"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\""+" /s /y ");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+ "\" %partD%PEASmartQ\\PEASmartQ\\Program\\Shortcut\\QPatchClient.lnk\""+ "\"C:\\Documents and Settings\\All Users\\Start Menu\\Programs\\Startup\\\""+" /s /y");
+                create.WriteLine("%systemroot%\\System32\\XCOPY "+ "\" %partD%PEASmartQ\\PEASmartQ\\Program\\Shortcut\\QPatchClient.lnk\""+ "\"%USERPROFILE%\\Start Menu\\Programs\\Startup\""+" /s /y");
+                create.WriteLine("");
+                create.Close();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\1. work\ฝีกงาน\test file Server.bat";
+
+            File.Create(path).Dispose();
+
+            using (TextWriter create = new StreamWriter(path))
+            {
+                create.WriteLine("@echo off");
+                create.WriteLine("set path=%1 %2 %3");
+                create.WriteLine(":วนลูปตัดค่าเป็น 2 ค่า");
+                create.WriteLine("for /f " + "\"tokens=1* delims=-\"" + " %%a in (" + "\"%path%\"" + ") do (");
+                create.WriteLine("set partF=%%a");
+                create.WriteLine("set partD=%%b");
+                create.WriteLine(")");
+                create.WriteLine(":ตัดค่าช่องว่างด้านหลัง" + " drive");
+                create.WriteLine("set partD=%partD%##");
+                create.WriteLine("set partD=%partD:                ##=##%");
+                create.WriteLine("set partD=%partD:        ##=##%");
+                create.WriteLine("set partD=%partD:    ##=##%");
+                create.WriteLine("set partD=%partD:  ##=##%");
+                create.WriteLine("set partD=%partD: ##=##%");
+                create.WriteLine("set partD=%partD:##=%");
+                create.WriteLine("");
+            }
+        }
+
+        private void btnmove_Click(object sender, EventArgs e)
+        {
+            Process.Start("E:\\1. work\\ฝีกงาน\\ตัวอย่าง - Copy\\PEASmartQ\\PEASmartQ\\Copytest.bat");
+        }
+    }
+}
